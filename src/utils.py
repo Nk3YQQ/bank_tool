d3fe25a3cddb67ebdd8da51915b2faeccf6ea801
Egor Nikitin <eganik5674@gmail.com>
2023-11-27 17:26:34 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import json
 import logging
+import re
+from collections import Counter
 from typing import Any, Optional
 
 from src.masks import mask_card_with_name
@@ -63,6 +65,56 @@ def get_kind_of_transaction(
         elif value == "PENDING":
             return [element for element in transactions if element["state"] == value]
     return [element for element in transactions if element["state"] == value]
+
+
+def get_info_about_transactions(string: str, filepath: str = "../data/transactions.json") -> list[dict]:
+    """
+    Функция принимает данные о транзакциях и строку поиска и возвращает список транзакций по описанию
+    транзакции, которая задана в строке поиска
+    """
+    with open(filepath, "r", encoding="utf-8") as file:
+        transactions = json.load(file)
+
+        matching_transactions = []
+
+        for transaction in transactions:
+            description = transaction.get("description")
+            if description and re.search(re.escape(string), description, re.IGNORECASE):
+                matching_transactions.append(transaction)
+
+        return matching_transactions
+
+
+def get_description_info(filepath: str = "../data/transactions.json") -> dict:
+    """
+    Функция создаёт словарь с категориями описаний транзакций в файле, который хранит список транзакций
+    """
+    data: dict = {}
+    with open(filepath, "r", encoding="utf-8") as file:
+        transactions = json.load(file)
+        for transaction in transactions:
+            description = transaction["description"]
+            if description not in data.keys():
+                data[description] = None
+        data["Без описания"] = data.pop(None)
+        return data
+
+
+def get_statistic_about_transactions(categories: dict, filepath: str = "../data/transactions.json") -> dict:
+    """
+    Функция принимает список словарей с данными о банковских операциях и словарь категорий операций и возвращать
+    словарь, в котором ключи — это названия категорий, а значения — это количество операций в каждой категории
+    """
+    with open(filepath, "r", encoding="utf-8") as file:
+        transactions = json.load(file)
+        list_of_description = [transaction["description"] for transaction in transactions]
+        counted = Counter(list_of_description)
+        categories["Перевод организации"] = counted["Перевод организации"]
+        categories["Перевод с карты на карту"] = counted["Перевод с карты на карту"]
+        categories["Открытие вклада"] = counted["Открытие вклада"]
+        categories["Перевод со счета на счет"] = counted["Перевод со счета на счет"]
+        categories["Без описания"] = counted[None]
+        return categories
 
 
 logger.info("utils.py is working. Status: ok")
